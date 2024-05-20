2c8fe378551e60afb38f88affa624a80d3b2e42d
athazayyan <atha.al.khand@gmail.com>
2024-05-20 18:50:49 +0700

    hitung huruf

--- ubahhuruf.c
+++ ubahhuruf.c
@@ -1,95 +1,93 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-    float nilai = 0;
+int jumlahnilaiKarakter(){
+    double nilai = 0;
     char inputan[500];
     
-    // Membaca inputan sampai newline
     scanf("%[^\n]", inputan);
     
-    // Mengiterasi setiap karakter dalam inputan
-    for (int i = 0; i < strlen(inputan); i++)
-    {
-        switch (inputan[i])
-        {
-            case 'a':
+    
+    for (int i = 0; i < strlen(inputan); i++) {
+        char c = inputan[i];
+        
+        switch (c) {
+            case 'a': case 'A':
                 nilai += (5 * 1 + 8) % 26;
                 break;
-            case 'b':
+            case 'b': case 'B':
                 nilai += (5 * 2 + 8) % 26;
                 break;
-            case 'c':
+            case 'c': case 'C':
                 nilai += (5 * 3 + 8) % 26;
                 break;
-            case 'd':
+            case 'd': case 'D':
                 nilai += (5 * 4 + 8) % 26;
                 break;
-            case 'e':
+            case 'e': case 'E':
                 nilai += (5 * 5 + 8) % 26;
                 break;
-            case 'f':
+            case 'f': case 'F':
                 nilai += (5 * 6 + 8) % 26;
                 break;
-            case 'g':
+            case 'g': case 'G':
                 nilai += (5 * 7 + 8) % 26;
                 break;
-            case 'h':
+            case 'h': case 'H':
                 nilai += (5 * 8 + 8) % 26;
                 break;
-            case 'i':
+            case 'i': case 'I':
                 nilai += (5 * 9 + 8) % 26;
                 break;
-            case 'j':
+            case 'j': case 'J':
                 nilai += (5 * 10 + 8) % 26;
                 break;
-            case 'k':
+            case 'k': case 'K':
                 nilai += (5 * 11 + 8) % 26;
                 break;
-            case 'l':
+            case 'l': case 'L':
                 nilai += (5 * 12 + 8) % 26;
                 break;
-            case 'm':
+            case 'm': case 'M':
                 nilai += (5 * 13 + 8) % 26;
                 break;
-            case 'n':
+            case 'n': case 'N':
                 nilai += (5 * 14 + 8) % 26;
                 break;
-            case 'o':
+            case 'o': case 'O':
                 nilai += (5 * 15 + 8) % 26;
                 break;
-            case 'p':
+            case 'p': case 'P':
                 nilai += (5 * 16 + 8) % 26;
                 break;
-            case 'q':
+            case 'q': case 'Q':
                 nilai += (5 * 17 + 8) % 26;
                 break;
-            case 'r':
+            case 'r': case 'R':
                 nilai += (5 * 18 + 8) % 26;
                 break;
-            case 's':
+            case 's': case 'S':
                 nilai += (5 * 19 + 8) % 26;
                 break;
-            case 't':
+            case 't': case 'T':
                 nilai += (5 * 20 + 8) % 26;
                 break;
-            case 'u':
+            case 'u': case 'U':
                 nilai += (5 * 21 + 8) % 26;
                 break;
-            case 'v':
+            case 'v': case 'V':
                 nilai += (5 * 22 + 8) % 26;
                 break;
-            case 'w':
+            case 'w': case 'W':
                 nilai += (5 * 23 + 8) % 26;
                 break;
-            case 'x':
+            case 'x': case 'X':
                 nilai += (5 * 24 + 8) % 26;
                 break;
-            case 'y':
+            case 'y': case 'Y':
                 nilai += (5 * 25 + 8) % 26;
                 break;
-            case 'z':
+            case 'z': case 'Z':
                 nilai += (5 * 26 + 8) % 26;
                 break;
             default:
