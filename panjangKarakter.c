b02228024ad7d25d9da45225ceac66efeb936f64
athazayyan <atha.al.khand@gmail.com>
2024-05-21 12:06:33 +0700

    hitung jumlah karakter

---
+++ panjangKarakter.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int hitunghasilKarakter() {
+    char tempkata;
+    FILE *fileHitungKarakter = fopen("input.txt", "r");
+    if (fileHitungKarakter == NULL) {
+        printf("File tidak dapat dibuka\n");
+        exit(1);
+    }
+    
+    int hasilKarakter = 0;
+    while ((tempkata = fgetc(fileHitungKarakter)) != EOF) {
+        hasilKarakter++;
+    }
+    
+    fclose(fileHitungKarakter);
+    printf("Jumlah karakter dalam file adalah %d\n", hasilKarakter);
+    return hasilKarakter;
+}
